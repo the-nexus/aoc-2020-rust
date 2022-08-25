12bdb01cf768659143218334051925ac4298dca7
the-nexus <the-nexus@github.com>
2022-08-25 19:03:58 -0400

    Personal Access Token test #5!

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,20 @@
-use aoc_runner_derive::{aoc/*, aoc_generator*/};
+use aoc_runner_derive::{aoc, aoc_generator};
 
-// #[aoc_generator(day1)]
-// fn parse_input(input: &str) -> str {
-//     return input;
-// }
+#[aoc_generator(day1)]
+fn parse_input(input: &str) -> Vec<i32> {
+    input.lines()
+        .map(|line| {
+            line.parse::<i32>().expect("")
+        })
+        .collect::<Vec<i32>>()
+}
 
 #[aoc(day1, part1)]
-fn part_one(_input: &str) -> i32 {
-    0
+fn part_one(input: &Vec<i32>) -> i32 {
+    input[0]
 }
 
 #[aoc(day1, part2)]
-fn part_two(_input: &str) -> i32 {
-    0
+fn part_two(input: &Vec<i32>) -> i32 {
+    input[0]
 }
